d9cea10090d3794af427fb1b20f9d5de21369294
Kate Ramsey <kateramsey@live.com>
2016-04-15 19:41:01 -0500

    Models

---
+++ server/Flashcards/Flashcards.Web/Models/Subject.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Flashcards.Web.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public virtual ICollection<Set> Sets { get; set; }
+    }
+}
