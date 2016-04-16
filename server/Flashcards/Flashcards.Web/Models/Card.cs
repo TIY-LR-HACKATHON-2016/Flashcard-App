a17aae45400d212b4c15f7a2488bc06d31cba5dc
Kate Ramsey <kateramsey@live.com>
2016-04-15 20:11:05 -0500

    Scaffold CardsController and default views. Writing to Db :)

--- server/Flashcards/Flashcards.Web/Models/Card.cs
+++ server/Flashcards/Flashcards.Web/Models/Card.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
 
 namespace Flashcards.Web.Models
 {
     public class Card
     {
         public int Id { get; set; }
-        //TODO: Required tags
+        [Required]
         public string frontText { get; set; }
-
+        [Required]
         public string backText { get; set; }
+      //  [Required]
         public Set Set { get; set; }
+       // [Required]
         public Subject Subject { get; set; }
     }
 }
