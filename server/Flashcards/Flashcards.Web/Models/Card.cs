681c363d15df8c99f148dd7535a2fbae0a349f30
Kate Ramsey <kateramsey@live.com>
2016-04-15 19:41:01 -0500

    Models

---
+++ server/Flashcards/Flashcards.Web/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Flashcards.Web.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        //TODO: Required tags
+        public string frontText { get; set; }
+
+        public string backText { get; set; }
+        public Set Set { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
