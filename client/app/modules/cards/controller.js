5bbf15ed4676a2f3530c840bc075b78d62c42263
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-16 12:01:47 -0500

    ...

--- client/app/modules/cards/controller.js
+++ client/app/modules/cards/controller.js
@@ -21,26 +21,26 @@ class FlashController {
 
 
   getData() {
-		// this._$http
-		// .get(`http://tiy-lr-flashcards.azurewebsites.net/cards/index`)
-		// .then((response) => {
-		// 	console.log(response);
-		// 	this.cards = response.data;
-		// });
+		this._$http
+		.get(`http://tiy-lr-flashcards.azurewebsites.net/cards/index`)
+		.then((response) => {
+			console.log(response);
+			this.cards = response.data;
+		});
 
 
 
 //TEST DATA -------------------------------------------------------------------
-			this.cards = [
-			{ id: 1,
-				frontText: "Title",
-				backText: "description"
-			},
-			{ id: 2,
-				frontText: "Title2",
-				backText: "description2"
-			}
-		]
+		// 	this.cards = [
+		// 	{ id: 1,
+		// 		frontText: "Title",
+		// 		backText: "description"
+		// 	},
+		// 	{ id: 2,
+		// 		frontText: "Title2",
+		// 		backText: "description2"
+		// 	}
+		// ]
 	//-------------------------------------------------------------------
 
   }
