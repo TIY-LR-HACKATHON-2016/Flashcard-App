70973da7e93fdeb4c5d8772450334535dd6d6927
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-15 22:17:22 -0500

    cards flipping

--- client/app/modules/cards/controller.js
+++ client/app/modules/cards/controller.js
@@ -1,35 +1,46 @@
 
 class FlashController {
 
-
 	constructor($http) {
     this._$http = $http;
+		// this.id = $stateParams.id;
+		this.cards = [];
 
 		this.getData();
 	}
 
 	toggleClass(card) {
-		card.flipped = true;
+		if (card.flipped === true) {
+			card.flipped = false;
+		}
+		else {
+			card.flipped = true;			
+		}
 	}
 
   getData() {
-		get(`tiy-lr-flashcards.azurewebsites.net/cards/details/1`)
-		// 	this.cards = [
-		// 	{ id: 1,
-		// 		frontText: "Title",
-		// 		backText: "description"
-		// 	},
-		// 	{ id: 2,
-		// 		frontText: "Title2",
-		// 		backText: "description2"
-		// 	}
-		// ]
+		// this._$http
+		// .get(`http://tiy-lr-flashcards.azurewebsites.net/cards/details/1`)
+		// .then((response) => {
+		// 	console.log(response);
+		// 	this.cards = response.data.cards;
+		// });
+
+			this.cards = [
+			{ id: 1,
+				frontText: "Title",
+				backText: "description"
+			},
+			{ id: 2,
+				frontText: "Title2",
+				backText: "description2"
+			}
+		]
 		// this._$http
 		// .get("New Flashcard")
 		// .then((response) => {
     //   console.log(results);
     //   console.log("working");
-		this.cards = response.data.cards;
 		// });
 
   }
