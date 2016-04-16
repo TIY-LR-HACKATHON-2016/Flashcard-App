be72d6d49def114360a2447bf2033fd7ec1e6555
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-04-15 21:48:52 -0500

    ...

--- client/app/modules/cards/controller.js
+++ client/app/modules/cards/controller.js
@@ -13,22 +13,23 @@ class FlashController {
 	}
 
   getData() {
-		this.cards = [
-			{ id: 1,
-				frontText: "Title",
-				backText: "description"
-			},
-			{ id: 2,
-				frontText: "Title2",
-				backText: "description2"
-			}
-		]
+		get(`tiy-lr-flashcards.azurewebsites.net/cards/details/1`)
+		// 	this.cards = [
+		// 	{ id: 1,
+		// 		frontText: "Title",
+		// 		backText: "description"
+		// 	},
+		// 	{ id: 2,
+		// 		frontText: "Title2",
+		// 		backText: "description2"
+		// 	}
+		// ]
 		// this._$http
 		// .get("New Flashcard")
 		// .then((response) => {
     //   console.log(results);
     //   console.log("working");
-		// this.cards = response.data.cards;
+		this.cards = response.data.cards;
 		// });
 
   }
